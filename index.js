7fcf473fb03919b364da226397c2529c563e7f40
jgouveia-lab <265724459+joaorobalogouveia@users.noreply.github.com>
2026-05-14 19:47:34 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + ` ` + s2 + ` ` + s3 + ` ` + s4 + ` ` + s5;
 
 // Print out the concatenated string
-
-
-
+// console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,11 +20,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result = part1.slice(0, -1) + part1.slice(-1).toUpperCase() + part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
-
+// 
 
 
 /*******************************************
@@ -35,10 +32,12 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipAmount = billTotal * 0.15;
+
 
 
 // Print out the tipAmount
-
+// console.log(tipAmount);
 
 
 
@@ -47,10 +46,13 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+
 
 
 // Print the generated random number
-
+// console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +64,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
+console.log(expression1);
 
 const expression2 = a || b;
+// true
+console.log(expression2);
 
 const expression3 = !a && b;
+// false
+console.log(expression3);
 
 const expression4 = !(a && b);
+// true
+console.log(expression4);
 
 const expression5 = !a || !b;
+// true 
+console.log(expression5);
 
 const expression6 = !(a || b);
+// false
+console.log(expression6);
 
 const expression7 = a && a;
+// true
+console.log(expression7);
